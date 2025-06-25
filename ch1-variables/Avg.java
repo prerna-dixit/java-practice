ebe367a8cb1048ddfc76384528dd2fb7fc9de9f4
prerna-dixit <prerna.dixit.pd.4@gmail.com>
2025-06-25 13:21:51 +0530

    Added Avg.java

---
+++ ch1-variables/Avg.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Avg 
+{public static void avg(int a, int b, int c) 
+    { double avg=((double)(a+b+c))/3;
+      System.out.println("average of these numbers is " + avg);
+    }
+    
+    public static void main(String[] args) 
+    { Scanner sc=new Scanner(System.in);
+        System.out.println("enter the numbers-");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        avg(a, b, c);
+
+    
+}
+    
+}
