a6529bda2d52016751abb9f9352927c01a308382
prerna-dixit <prerna.dixit.pd.4@gmail.com>
2025-06-25 13:01:40 +0530

    Added SumProd.java

---
+++ ch1-variables/SumProd.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class SumProd 
+{public static void sum(int a, int b) 
+    {int sum=a+b;
+        System.out.println("sum of " + a + " and " + b + " is " + sum );
+    }
+ public static void prod(int a, int b) 
+    {int prod=a*b;
+        System.out.println("product of " + a + " and " + b + " is " + prod);
+    }
+    public static void main(String[] args) 
+    { Scanner sc=new Scanner(System.in);
+    System.out.println("Enter two numbers-");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    sum(a, b);
+    prod(a, b);
+    }
+    
+}
