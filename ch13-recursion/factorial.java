c88c3e6fcef2f4bad7280bc0027b365313bf905b
Prerna Dixit <prerna.dixit.pd.4@gmail.com>
2026-08-21 20:10:07 +0530

    factorial recursion

---
+++ ch13-recursion/factorial.java
@@ -0,0 +1,17 @@
+public class factorial {
+    public static int fact(int n)
+    {
+        //base class
+        if(n==0)
+        {
+            return 1;
+        }
+        
+        return n*fact(n-1);
+    }    
+
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(fact(n));
+    }
+}
