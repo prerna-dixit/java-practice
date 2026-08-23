7af15b98efa1973dfcc719f8aa1956f6acece8db
Prerna Dixit <prerna.dixit.pd.4@gmail.com>
2026-08-23 12:14:31 +0530

    exponent recursion

---
+++ ch13-recursion/exponent.java
@@ -0,0 +1,11 @@
+public class exponent {
+    public static int exp(int x, int n)
+    {
+        if(n==1)
+        {return x;}
+        return x*exp(x, n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(exp(2, 4));
+    }
+}
